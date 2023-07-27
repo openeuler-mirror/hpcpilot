e4d8e2a546e970c5131fb481b287cd97789980b5
y30024638 <hao844634884@163.com>
2023-07-27 19:12:15 +0800

    典型模板abaqus

---
+++ donau-app-template/abaqus/common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (c) Huawei Technologies Co., Ltd. 2018-2022. All rights reserved.
+# Common functions for template.
+
+# Application running command.
+
+SCRIPT_PATH="/share/software/scripts/app/abaqus.sh"
+APP_CMD="${SCRIPT_PATH} ${CPU_CORES} ${CASE_FILE}"
